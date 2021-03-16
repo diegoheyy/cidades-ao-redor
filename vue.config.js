53db47240bcd1983e5b548b6611c7cf15bfb4507
Diego Alberto Hey <diegoheyy@gmail.com>
2021-03-16 16:31:32 -0300

    change title and icon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Cidades ao Redor";
+            return args;
+        })
+}
 }
